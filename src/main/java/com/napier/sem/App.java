9e7b5fe4bc05e201c7d0132d2206d5a04fd6544f
PPSawyer <patrickpsawyer@gmail.com>
2021-02-22 02:27:39 -0500

    Add the world

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -11,10 +11,12 @@ public class App
 
         // Connect to database
         a.connect();
+
         //Get Employee
-       // Employee emp = a.getEmployee(110567);
+         TheWorld emp = a.getName(55);
+
         //Display results
-      //  a.displayEmployee(emp);
+        a.displayWorld(emp);
 
         // Disconnect from database
         a.disconnect();
@@ -51,10 +53,10 @@ public class App
                 Thread.sleep(30000);
 
                 //Running Locally Not in Docker
-                //con = DriverManager.getConnection("jdbc:mysql://localhost:33060/world?useSSL=true", "root", "example");
+                con = DriverManager.getConnection("jdbc:mysql://localhost:33060/world?useSSL=true", "root", "example");
 
                 // Connect to database inside docker
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
+                //con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
 
                 //con = DriverManager.getConnection("", "root", "example");
 
@@ -92,4 +94,55 @@ public class App
         }
     }
 
+    public TheWorld getName(int ID)
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT country.Population, Continent, Region,Name "
+                            + "FROM country "
+                            + "WHERE Capital = " + ID;
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new employee if valid.
+            // Check one is returned
+            if (rset.next())
+            {
+                TheWorld emp = new TheWorld();
+                emp.Population = rset.getLong("Population");
+                emp.Continent = rset.getString("Continent");
+                emp.Region = rset.getString("Region");
+                emp.Name = rset.getString("Name");
+              //  emp.District = rset.getString("District");
+               // emp.Name = rset.getString("Name");
+                return emp;
+            }
+            else
+                return null;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get the world details");
+            return null;
+        }
+    }
+
+    public void displayWorld(TheWorld emp)
+    {
+        if (emp != null)
+        {
+            System.out.println(
+                    emp.Capital + " "
+                            + emp.Continent + " "
+                            + emp.Region + "\n"
+                            + emp.Name + "\n");
+                           // + "Salary:" + emp.salary + "\n"
+                           // + emp.dept_name + "\n"
+                           // + "Manager: " + emp.manager + "\n");
+        }
+    }
 }
