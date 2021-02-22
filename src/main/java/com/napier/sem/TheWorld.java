dda9c39819c26fd3404b4b5a5c6dd0428c5a56cb
PPSawyer <patrickpsawyer@gmail.com>
2021-02-22 02:27:39 -0500

    Add the world

---
+++ src/main/java/com/napier/sem/TheWorld.java
@@ -0,0 +1,102 @@
+package com.napier.sem;
+/**
+ * Represents an employee
+ */
+public class TheWorld {
+    /**
+     * Country code
+     */
+    public String Code;
+
+    /**
+     * country name
+     */
+    public String Name;
+
+    /**
+     * Country continent
+     */
+    public String Continent;
+
+    /**
+     * Country Region
+     */
+    public String Region;
+
+    /**
+     * Country Population
+     */
+    public long Population;
+
+    /**
+     * Country Capital
+     */
+    public int Capital;
+
+    /**
+     * Country Head of State
+     */
+    public String HeadOfState;
+
+    /**
+     * Country Government Form
+     */
+    public String GovernmentForm;
+
+    /**
+     * Country Code2
+     */
+    public int Code2;
+
+    /**
+     * Country GNP
+     */
+    public int GNP;
+
+    /**
+     * Country GNPOld
+     */
+    public int GNPOld;
+
+    /**
+     * Country LifeExpectancy
+     */
+    public int LifeExpectancy;
+
+    /**
+     * Country Surface Area
+     */
+    public int SurfaceArea;
+
+    /**
+     * Country IndepYear
+     */
+    public int IndepYear;
+
+    /**
+     * City ID
+     */
+    public int ID;
+
+    /**
+     * City CountryCode
+     */
+    public String CountryCode;
+
+    /**
+     * City District
+     */
+    public String District;
+
+    /**
+     * City Name
+     */
+   // public String Name;
+
+    /**
+     * City Population
+     */
+    //public int Population;
+
+}
+
