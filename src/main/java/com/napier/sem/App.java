c140967026e4e5f8741b33355dc2c1c068b7769f
PPSawyer <patrickpsawyer@gmail.com>
2021-04-18 17:05:49 -0400

    Deployment

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -134,7 +134,7 @@ public class App
                  //     con = DriverManager.getConnection("jdbc:mysql://localhost:33060/world?useSSL=true", "root", "example");
 
                 // Connect to database inside docker
-                 con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
+               con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
 
                 System.out.println("Successfully connected to the World Database");
                 break;
