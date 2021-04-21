96a5a5a0eabbd7e5af6878c9575d5dc36f52c6cf
LKelsick <40517785@live.napier.ac.uk>
2021-04-21 03:46:06 -0400

    Added class theWorld to correct error.

---
+++ src/main/java/com/napier/sem/TheWorld.java
@@ -0,0 +1,9 @@
+package com.napier.sem;
+
+public class TheWorld {
+    public Long Population;
+    public String Continent;
+    public String Region;
+    public String Name;
+    public int Capital;
+}
